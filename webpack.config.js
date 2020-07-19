233b6d824789b10806e707416eae41c156c6decf
syu15 <sarahmyu15@gmail.com>
2020-07-19 16:20:31 -0700

    Fix version on emberVersion

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './packages/ember-debug-global/src/index.ts',
   devtool: 'inline-source-map',
   module: {
